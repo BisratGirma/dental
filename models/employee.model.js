032ec2175f1fc10d0a7039604ce48fc37c615fff
bisrat <bisratx@gmail.com>
2024-02-03 22:36:33 +0300

    models added

---
+++ models/employee.model.js
@@ -0,0 +1,10 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const EmployeeSchema = new Schema({
+  fullName: { type: String, required: true },
+  department: { type: Schema.Types.ObjectId, required: true },
+  createdAt: { type: Date },
+});
+
+module.exports = mongoose.model("Employee", EmployeeSchema, "employees");
