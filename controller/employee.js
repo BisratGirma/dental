bb244904202a5eb25e78e9d78871b2d3c1954494
bisrat <bisratx@gmail.com>
2024-02-04 12:54:47 +0300

    employee controller basics

--- controller/employee.js
+++ controller/employee.js
@@ -1,34 +1,108 @@
 const { default: mongoose } = require("mongoose");
 const Employee = require("../models/employee.model");
+const Department = require("../models/department.model");
 
-exports.createEmployee = (req, res) => {
-  res.send("create");
+exports.createEmployee = async (req, res) => {
+  try {
+    const { name, departmentID } = req.body;
+
+    if (typeof name !== "string")
+      return res.status(400).json({ message: "Name is required!" });
+    if (mongoose.Types.ObjectId.isValid(departmentID) !== true)
+      return res.status(400).json({ message: "department ID is not correct!" });
+
+    const department = await Department.findById(departmentID);
+
+    const employee = await Employee.create({ name, createdAt: new Date() });
+
+    res.status(201).json({
+      status: "success",
+      data: {
+        employee,
+      },
+    });
+  } catch (error) {
+    res.status(400).json({ message: "please, try again later!" });
+  }
 };
 
 exports.getEmployee = async (req, res) => {
-  const { id } = req.params;
+  try {
+    const { id } = req.params;
 
-  if (mongoose.Types.ObjectId.isValid(id))
-    return res.status(400).json({ message: "Employee ID not correct!" });
+    if (mongoose.Types.ObjectId.isValid(id) === false)
+      return res.status(400).json({ message: "Employee ID not correct!" });
 
-  const employee = await Employee.findById(id);
+    const employee = await Employee.findById(id);
 
-  res.status(200).json({
-    status: "success",
-    data: {
-      employee,
-    },
-  });
+    res.status(200).json({
+      status: "success",
+      data: {
+        employee,
+      },
+    });
+  } catch (error) {
+    res.status(400).json({ message: "please, try again later!" });
+  }
 };
 
-exports.getAllEmployee = (req, res) => {
-  res.send("readAll");
+exports.getAllEmployee = async (req, res) => {
+  try {
+    const employee = await Employee.find();
+
+    res.status(200).json({
+      status: "success",
+      data: {
+        employee,
+      },
+    });
+  } catch (error) {
+    res.status(400).json({ message: "please, try again later!" });
+  }
 };
 
-exports.updateEmployee = (req, res) => {
-  res.send("update");
+exports.updateEmployee = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const { name } = req.body;
+
+    if (mongoose.Types.ObjectId.isValid(id) === false)
+      return res.status(400).json({ message: "Employee ID not correct!" });
+    if (typeof name !== "string")
+      return res.status(400).json({ message: "Name is required!" });
+    const employee = await Employee.findByIdAndUpdate(
+      id,
+      { name },
+      { new: true }
+    );
+
+    res.status(200).json({
+      status: "success",
+      data: {
+        employee,
+      },
+    });
+  } catch (error) {
+    res.status(400).json({ message: "please, try again later!" });
+  }
 };
 
-exports.deleteEmployee = (req, res) => {
-  res.send("delete");
+exports.deleteEmployee = async (req, res) => {
+  try {
+    const { id } = req.params;
+
+    if (mongoose.Types.ObjectId.isValid(id) === false)
+      return res.status(400).json({ message: "Employee ID not correct!" });
+
+    const result = await Employee.findByIdAndDelete(id);
+
+    res.status(200).json({
+      status: "success",
+      data: {
+        result,
+      },
+    });
+  } catch (error) {
+    res.status(400).json({ message: "please, try again later!" });
+  }
 };
